a2b9ebdb5a2da9d39793f02e244d03f066682e05
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2024-09-07 09:46:28 +0530

    Responding with data. identify headers and some response header

--- NODEHTTP/server.js
+++ NODEHTTP/server.js
@@ -1,9 +1,20 @@
 const http = require('http');
 const PORT = 4000;
 
+const todo = [
+  { id: 1, text: 'Todo 1' },
+  { id: 2, text: 'Todo 2' },
+];
+
 const server = http.createServer((req, res) => {
-  console.log(req);
-  res.end();
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('X-Powered-By', 'Node.js');
+  res.end(
+    JSON.stringify({
+      success: true,
+      data: todo,
+    })
+  );
 });
 
 server.listen(PORT, () => {
