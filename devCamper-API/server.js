206d0d78c9e46f51e225332ce94bb224dea23fc5
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2024-09-07 17:45:42 +0530

    connecting to the databaase with mongoose, add handled unhadled promise rejection

--- devCamper-API/server.js
+++ devCamper-API/server.js
@@ -2,12 +2,16 @@ const express = require('express');
 const dotenv = require('dotenv');
 const bootcampRouter = require('../devCamper-API/routes/bootcamp');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 // load the env
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Database connection
+connectDB();
 
 const logger = (req, res, next) => {
   req.hello = 'Hello world'; // can create variable or something during the request
@@ -22,6 +26,13 @@ if (process.env.NODE_ENV === 'development') {
 // Mount routes
 app.use('/api/v1/bootcamp', bootcampRouter);
 
-app.listen(PORT, () =>
+const server = app.listen(PORT, () =>
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handled unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server and exit process
+  server.close(() => process.exit(1));
+});
