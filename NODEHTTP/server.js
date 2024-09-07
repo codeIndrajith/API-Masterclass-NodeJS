6c721f5ed73b84b2fcd55fe8fc94c546faf4889f
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2024-09-07 11:27:32 +0530

    HTTP method and RESTfull api

--- NODEHTTP/server.js
+++ NODEHTTP/server.js
@@ -7,8 +7,7 @@ const todo = [
 ];
 
 const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('X-Powered-By', 'Node.js');
+  const { method, url } = req;
   let body = [];
 
   // using express we can set req.body.name , req.body.email like this. but using http module we can use to chunk and Buffer method to get the body data
@@ -18,14 +17,36 @@ const server = http.createServer((req, res) => {
     })
     .on('end', () => {
       body = Buffer.concat(body).toString();
-      console.log(body);
+
+      let status = 404;
+      const response = {
+        success: false,
+        data: null,
+      };
+
+      if (method === 'GET' && url === '/todos') {
+        status = 200;
+        response.success = true;
+        response.data = todo;
+      } else if (method === 'POST' && url === '/todos') {
+        const { id, text } = JSON.parse(body);
+        if (!id || !text) {
+          status = 400;
+          response.error = 'Add id and text field';
+        } else {
+          todo.push({ id, text });
+          status = 201;
+          response.success = true;
+          response.data = todo;
+        }
+      }
+      res.writeHead(status, {
+        'Content-Type': 'application/json',
+        'X-Powered-By': 'Node.js',
+      });
+
+      res.end(JSON.stringify(response));
     });
-  res.end(
-    JSON.stringify({
-      success: true,
-      data: todo,
-    })
-  );
 });
 
 server.listen(PORT, () => {
