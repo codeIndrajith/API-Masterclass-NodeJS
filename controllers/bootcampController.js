18ec36efdd2f63e4ed591aec26d233dca42aad20
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2024-09-10 16:46:08 +0530

    Find bootcamp, update bootcamp and delete bootcamp controls create success

--- controllers/bootcampController.js
+++ controllers/bootcampController.js
@@ -1,7 +1,7 @@
 const BootcampModel = require('../models/bootcampModel');
 
 // @desc     Get all bootcamps
-// @route    GET / .api/v1/bootcamps
+// @route    GET /api/v1/bootcamps
 // @access   Public
 
 const getAllBootcamps = async (req, res, next) => {
@@ -13,8 +13,20 @@ const getAllBootcamps = async (req, res, next) => {
   }
 };
 
+// @desc     Find bootcamp
+// @route    GET /api/v1/bootcamps/:id
+// @access   Public
+const findBootcamp = async (req, res, next) => {
+  const bootcamp = await BootcampModel.findById(req.params.id);
+  if (!bootcamp) {
+    res.status(404).json({ success: false, msg: 'Bootcamp Not Found' });
+  } else {
+    res.status(200).json({ success: true, data: bootcamp });
+  }
+};
+
 // @desc     Add bootcamps
-// @route    POST / .api/v1/bootcamps
+// @route    POST /api/v1/bootcamps
 // @access   Private
 const addBootcamps = async (req, res, next) => {
   try {
@@ -27,4 +39,43 @@ const addBootcamps = async (req, res, next) => {
   }
 };
 
-module.exports = { getAllBootcamps, addBootcamps };
+// @desc     Update bootcamp
+// @route    PUT /api/v1/bootcamps/:id
+// @access   Private
+const updateBootcamp = async (req, res, next) => {
+  const bootcamp = await BootcampModel.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+  if (!bootcamp) {
+    res.status(404).json({ success: false, msg: 'Bootcamp Not Found' });
+  } else {
+    res
+      .status(200)
+      .json({ success: true, msg: 'Bootcamp Update Success', data: bootcamp });
+  }
+};
+
+// @desc     Delete bootcamp
+// @route    DELETE /api/v1/bootcamps/:id
+// @access   Private
+const deleteBootcamp = async (req, res, next) => {
+  const bootcamp = await BootcampModel.findByIdAndDelete(req.params.id);
+  if (!bootcamp) {
+    res.status(404).json({ success: false, msg: 'Bootcamp Not Found' });
+  } else {
+    res.status(200).json({ success: true, msg: 'Bootcamp Delete Success' });
+  }
+};
+
+module.exports = {
+  getAllBootcamps,
+  findBootcamp,
+  addBootcamps,
+  updateBootcamp,
+  deleteBootcamp,
+};
--- routes/bootcampRoutes.js
+++ routes/bootcampRoutes.js
@@ -2,9 +2,17 @@ const express = require('express');
 const {
   getAllBootcamps,
   addBootcamps,
+  updateBootcamp,
+  deleteBootcamp,
+  findBootcamp,
 } = require('../controllers/bootcampController');
 const router = express.Router();
 
 router.route('/').get(getAllBootcamps).post(addBootcamps);
+router
+  .route('/:id')
+  .get(findBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
 module.exports = router;
