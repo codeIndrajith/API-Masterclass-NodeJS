136f97f1fa3d03c0cb4c89c694dbc80c462fe93e
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2024-09-12 17:29:11 +0530

    Mongoose error handling part 2 | duplicate field and validation errors handle success

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -4,12 +4,24 @@ const errorHandler = (err, req, res, next) => {
   let error = { ...err }; // copy of all property of err object
   error.message = err.message;
   // Log to console for dev
-  console.log(err.stack);
+  console.log(err);
 
   // Mongoose bad Object
   if (err.name === 'CastError') {
     const message = `Bootcamp not found with id of ${err.value}`;
     error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose Duplicate Key
+  if (err.code === 11000) {
+    const message = 'Duplicate field value entered';
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose Validation Error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
   }
 
   res.status(error.statusCode || 500).json({
