89b2b640fd36f648124f7768f3abea7305912185
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2024-09-13 13:21:50 +0530

    GeoJSON location & Geocoder Hook. I use locationIQ api to get the location data

--- models/bootcampModel.js
+++ models/bootcampModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const bootcampSchema = mongoose.Schema(
   {
@@ -116,6 +117,25 @@ const bootcampSchema = mongoose.Schema(
 // Create Bootcamp slug from a name
 bootcampSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// Geocode & create location field
+bootcampSchema.pre('save', async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].state,
+    zipcode: loc[0].zipcode,
+    country: loc[0].country,
+  };
+
+  // Do not saved address in DB
+  this.address = undefined;
   next();
 });
 
