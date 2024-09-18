bfc29f04e0a1c2a57140d7e0a595d2118ca1f782
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2024-09-18 11:26:11 +0530

    User register & Encrypt password

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,6 +7,14 @@ const User = require('../models/userModel');
 // @access   Public
 
 const register = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+  });
   res.status(200).json({ success: true });
 });
 
